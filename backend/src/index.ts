6d3084b6a488f8e9d1d7d397f1a26779481b5608
Nikhil Bhagoria <nikhilkumar2450@gmail.com>
2025-06-05 22:49:51 +0530

    Add: Rate Limit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,26 @@
 import express from 'express';
-
+import rateLimit from 'express-rate-limit';
 const app = express;
 const PORT = 3000;
+
+app.use(express.json());
+
+// Rate Limiter configuration
+const otpLimiter = rateLimit({
+    windowMs:5*60*1000,
+    max:3,
+    message:'Too many requests, please try again after 5 minutes',
+    standardHeaders:true,
+    legacyHeaders:false,
+});
+
+const passwordResetLimiter = rateLimit({
+    windowMs:15*60*1000,
+    max:5,
+    message:'Too many password reset attempts, please try again after 15 minutes',
+    standardHeaders:true,
+    legacyHeaders:false,
+})
 
 const otpStore : Record<string,string> = {};
 
